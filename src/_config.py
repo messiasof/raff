b533d6a3c59b69d2ee439127a3326fdaf13f4ce1
Emanuel Messias <emanuel.messias@ibm.com>
2025-11-05 01:06:30 -0300

    Version 1.1

--- src/_config.py
+++ src/_config.py
@@ -5,5 +5,5 @@ URL = f"https://pastebin.com/raw/4YJG825M"
 
 baseDir = Path(__file__).resolve().parent
 
-DEVICE1 = "Ethernet"
-DEVICE2 = "Wi-Fi"
+DEVICE1 = "Ethernet" # Mude para o nome do dispositivo de rede que aparece no netsh / Change to network device's name that appears on netsh command
+DEVICE2 = "Wi-Fi" # Mude para o nome do dispositivo de rede que aparece no netsh / Change to network device's name that appears on netsh command
